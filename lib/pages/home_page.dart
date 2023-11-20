88d5441ab8cff920408b8db697de342e898c745a
sadid369 <sadid.jones@gmail.com>
2023-11-20 20:48:03 +0600

    updated

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,5 +1,6 @@
 import 'package:chat_app_firebase/pages/chat_page.dart';
 import 'package:chat_app_firebase/services/auth/auth_service.dart';
+import 'package:chat_app_firebase/widgets/msg_title.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:dotted_border/dotted_border.dart';
 import 'package:firebase_auth/firebase_auth.dart';
@@ -24,61 +25,73 @@ class _HomePageState extends State<HomePage> {
       'imgUrl':
           'https://images.unsplash.com/photo-1534528741775-53994a69daeb?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OHx8ZmFrZSUyMG1hbiUyMHdvbWFufGVufDB8fDB8fHww',
       'name': 'Aiony',
+      'msg': 'In ullamco cupidatat'
     },
     {
       'imgUrl':
           'https://images.unsplash.com/photo-1464863979621-258859e62245?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8N3x8ZmFrZSUyMG1hbiUyMHdvbWFufGVufDB8fDB8fHww',
       'name': 'Alexa',
+      'msg': 'In ullamco cupidatat'
     },
     {
       'imgUrl':
           'https://images.unsplash.com/photo-1539571696357-5a69c17a67c6?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NDd8fGZha2UlMjBtYW4lMjB3b21hbnxlbnwwfHwwfHx8MA%3D%3D',
       'name': 'Ian',
+      'msg': 'In ullamco cupidatat'
     },
     {
       'imgUrl':
           'https://images.unsplash.com/photo-1534528741775-53994a69daeb?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OHx8ZmFrZSUyMG1hbiUyMHdvbWFufGVufDB8fDB8fHww',
       'name': 'Aiony',
+      'msg': 'In ullamco cupidatat'
     },
     {
       'imgUrl':
           'https://images.unsplash.com/photo-1464863979621-258859e62245?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8N3x8ZmFrZSUyMG1hbiUyMHdvbWFufGVufDB8fDB8fHww',
       'name': 'Alexa',
+      'msg': 'In ullamco cupidatat'
     },
     {
       'imgUrl':
           'https://images.unsplash.com/photo-1539571696357-5a69c17a67c6?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NDd8fGZha2UlMjBtYW4lMjB3b21hbnxlbnwwfHwwfHx8MA%3D%3D',
       'name': 'Ian',
+      'msg': 'In ullamco cupidatat'
     },
     {
       'imgUrl':
           'https://images.unsplash.com/photo-1534528741775-53994a69daeb?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OHx8ZmFrZSUyMG1hbiUyMHdvbWFufGVufDB8fDB8fHww',
       'name': 'Aiony',
+      'msg': 'In ullamco cupidatat'
     },
     {
       'imgUrl':
           'https://images.unsplash.com/photo-1464863979621-258859e62245?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8N3x8ZmFrZSUyMG1hbiUyMHdvbWFufGVufDB8fDB8fHww',
       'name': 'Alexa',
+      'msg': 'In ullamco cupidatat'
     },
     {
       'imgUrl':
           'https://images.unsplash.com/photo-1539571696357-5a69c17a67c6?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NDd8fGZha2UlMjBtYW4lMjB3b21hbnxlbnwwfHwwfHx8MA%3D%3D',
       'name': 'Ian',
+      'msg': 'In ullamco cupidatat'
     },
     {
       'imgUrl':
           'https://images.unsplash.com/photo-1534528741775-53994a69daeb?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OHx8ZmFrZSUyMG1hbiUyMHdvbWFufGVufDB8fDB8fHww',
       'name': 'Aiony',
+      'msg': 'In ullamco cupidatat'
     },
     {
       'imgUrl':
           'https://images.unsplash.com/photo-1464863979621-258859e62245?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8N3x8ZmFrZSUyMG1hbiUyMHdvbWFufGVufDB8fDB8fHww',
       'name': 'Alexa',
+      'msg': 'In ullamco cupidatat'
     },
     {
       'imgUrl':
           'https://images.unsplash.com/photo-1539571696357-5a69c17a67c6?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NDd8fGZha2UlMjBtYW4lMjB3b21hbnxlbnwwfHwwfHx8MA%3D%3D',
       'name': 'Ian',
+      'msg': 'In ullamco cupidatat'
     },
   ];
   bool snackBarCalled = false;
@@ -125,39 +138,91 @@ class _HomePageState extends State<HomePage> {
               height: 20,
             ),
             Container(
+              alignment: Alignment.center,
               width: double.infinity,
-              height: 50,
+              height: 100,
               child: ListView.builder(
                 scrollDirection: Axis.horizontal,
                 itemCount: profileList.length,
                 itemBuilder: (context, index) {
                   return index == 0
-                      ? Container(
-                          alignment: Alignment.center,
-                          width: 60,
-                          height: 60,
-                          child: DottedBorder(
-                              padding: EdgeInsets.all(15),
-                              strokeWidth: 3,
-                              color: Colors.grey,
-                              radius: Radius.circular(30),
-                              child: Icon(
-                                Icons.add,
-                              )),
+                      ? Column(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          children: [
+                            Container(
+                                margin: EdgeInsets.only(left: 10),
+                                decoration: BoxDecoration(
+                                    shape: BoxShape.circle,
+                                    border: Border.all(
+                                      color: Colors.grey.shade300,
+                                    )),
+                                alignment: Alignment.center,
+                                width: 60,
+                                height: 60,
+                                child: Icon(
+                                  Icons.add,
+                                )),
+                          ],
                         )
-                      : Container(
-                          margin: EdgeInsets.only(left: 10),
-                          width: 60,
-                          height: 60,
-                          child: ClipRRect(
-                            borderRadius: BorderRadius.circular(30),
-                            child: Image.network(
-                              profileList[index]['imgUrl'],
-                              fit: BoxFit.cover,
+                      : Column(
+                          children: [
+                            Container(
+                              margin: EdgeInsets.only(left: 10),
+                              width: 60,
+                              height: 60,
+                              child: ClipRRect(
+                                borderRadius: BorderRadius.circular(30),
+                                child: Image.network(
+                                  profileList[index]['imgUrl'],
+                                  fit: BoxFit.cover,
+                                ),
+                              ),
                             ),
-                          ),
+                            SizedBox(
+                              height: 5,
+                            ),
+                            Text(
+                              profileList[index]['name'],
+                              style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                              ),
+                            )
+                          ],
                         );
                 },
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.symmetric(horizontal: mWidth * 0.03),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: const [
+                  Text(
+                    'Chats',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  Icon(Icons.more_horiz)
+                ],
+              ),
+            ),
+            SizedBox(
+              height: mHeight * 0.03,
+            ),
+            Expanded(
+              child: Padding(
+                padding: EdgeInsets.symmetric(horizontal: mWidth * 0.03),
+                child: ListView.builder(
+                  itemCount: profileList.length,
+                  itemBuilder: (context, index) {
+                    return MsgTitle(
+                      imgUrl: profileList[index]['imgUrl'],
+                      name: profileList[index]['name'],
+                      msg: profileList[index]['msg'],
+                    );
+                  },
+                ),
               ),
             ),
           ],
---
+++ lib/widgets/msg_title.dart
@@ -0,0 +1,93 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'package:flutter/material.dart';
+
+class MsgTitle extends StatefulWidget {
+  String imgUrl;
+  String name;
+  String msg;
+
+  MsgTitle({
+    Key? key,
+    required this.imgUrl,
+    required this.name,
+    required this.msg,
+  }) : super(key: key);
+
+  @override
+  _MsgTitleState createState() => _MsgTitleState();
+}
+
+class _MsgTitleState extends State<MsgTitle> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.only(bottom: 5),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          //leading Image
+          Row(
+            children: [
+              Container(
+                width: 50,
+                height: 50,
+                child: ClipRRect(
+                    borderRadius: BorderRadius.circular(25),
+                    child: Image.network(
+                      widget.imgUrl,
+                      fit: BoxFit.cover,
+                    )),
+              ),
+              SizedBox(
+                width: 10,
+              ),
+              //body
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  //Name of the person
+                  Text(
+                    widget.name,
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  //actual msg summary
+                  Text(
+                    widget.msg,
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      color: Colors.grey,
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+
+          //trialing
+          Column(
+            children: [
+              Text('02:11'),
+              Container(
+                alignment: Alignment.center,
+                width: 25,
+                height: 25,
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  color: Colors.amber.shade500,
+                ),
+                child: Text(
+                  '2',
+                  style: TextStyle(
+                    fontSize: 10,
+                  ),
+                ),
+              )
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
